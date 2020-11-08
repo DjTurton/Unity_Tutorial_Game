23a8489724fc10da91b9e37cc66f7d681203e88e
Turton <dturton68@gmail.com>
2020-11-08 22:56:03 +1300

    Created rocket and basic controls. #implement

---
+++ Assets/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    //variable to initialize rigidbody
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    //processes the players input
+    private void ProcessInput() 
+    {
+        //thrust
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+        
+        //left and right rotation
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+        
+    }
+}
