9061e0c817fa3ecdc75188d6cb9cdbfe16518440
Turton <dturton68@gmail.com>
2020-11-10 00:52:07 +1300

    Locked control to the XY axis, rotation on Z axis.
    Added basic logic for dealing with collisions - no actual gameplay consequence yet.
    #implement

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    
+    //this lets us modify the value of this float from the editor
+    [SerializeField] float rcsThrust = 100f;
 
     //variable to initialize rigidbody
     Rigidbody rigidbody;
@@ -19,6 +22,12 @@ public class Rocket : MonoBehaviour
     //processes the players input
     private void ProcessInput() 
     {
+        processThrust();
+        processRotation();
+    }
+
+    void processThrust()
+    {
         //thrust
         if (Input.GetKey(KeyCode.Space))
         {
@@ -32,15 +41,44 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
-        
+
+    }
+
+    void processRotation() 
+    {
         //left and right rotation
+        rigidbody.freezeRotation = true; // take manual control of rotation
+
+        //used for modifying how far we rotate per frame
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidbody.freezeRotation = false; // resume physics control of rotation
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing
+                break;
+            case "Fuek":
+                //refuel
+                break;
+            default:
+                //die
+                print("oh no"); //todo remove me!!!
+                break;
         }
     }
 
