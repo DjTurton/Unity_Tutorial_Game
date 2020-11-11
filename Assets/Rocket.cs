7435f186aeb3f854f47424bed3830738b9a9a029
Turton <dturton68@gmail.com>
2020-11-11 18:48:11 +1300

    Transitions
    - Added level transitions.
    - Added death
    - Controls disabled when dead/moving to next level
    #implement

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
@@ -11,6 +12,12 @@ public class Rocket : MonoBehaviour
     //variable to initialize rigidbody
     Rigidbody rigidbody;
     AudioSource audioSource;
+
+    //player state
+    enum State { Alive, Dying, Transcending };
+    State state = State.Alive;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +29,11 @@ public class Rocket : MonoBehaviour
     //processes the players input
     private void ProcessInput() 
     {
-        processThrust();
-        processRotation();
+        if (state == State.Alive) 
+        {
+            processThrust();
+            processRotation();
+        } 
     }
 
     void processThrust()
@@ -67,25 +77,44 @@ public class Rocket : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive)
+        {
+            return;
+        }
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 //do nothing
                 break;
-            case "Fuek":
-                //refuel
+            case "Finish":
+                //go to the next level
+                state = State.Transcending;
+                Invoke("LoadNextScene", 0.5f); //this gives us a short delay before loading the next level 
                 break;
             default:
                 //die
-                print("oh no"); //todo remove me!!!
+                state = State.Dying;
+                Invoke("LoadFirstScene", 0.5f);
                 break;
         }
+    }
+
+    //loads the next level
+    void LoadNextScene() 
+    {
+        SceneManager.LoadScene(1); //TODO: allow more levels 
+    }
+
+    //loads the first level
+    void LoadFirstScene() 
+    {
+        SceneManager.LoadScene(0); 
     }
 
     // Update is called once per frame
     void Update()
     {
         ProcessInput();
-        
     }
 }
